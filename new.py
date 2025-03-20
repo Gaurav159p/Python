b6ac6634c41f9940c6976c68f62180ce451cc56f
Gaurav26p <gauravprasad808@gmail.com>
2025-03-20 17:42:55 +0530

    message

--- new.py
+++ new.py
@@ -1,14 +1,14 @@
-# n = int(input())
-# answer = 0
-# for i in range(n+1):
-#     answer = answer + i*i
-# print(answer)
+n = int(input())
+answer = 0
+for i in range(n+1):
+    answer = answer + i*i
+print(answer)
 
 
-# n = int(input())
-# answer = 0
-# for i in range(n):
-#     m = int(input())
-#     answer = answer + m
-# answer = answer/n
-# print(answer)
+n = int(input())
+answer = 0
+for i in range(n):
+    m = int(input())
+    answer = answer + m
+answer = answer/n
+print(answer)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+n = int(input())
+# answer = 0
+# for i in range(n+1):
+#     answer = answer + i*i
+# print(answer)
+
